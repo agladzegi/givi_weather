5e5e3cf998fb046bfe721f41e6896e0433b896d0
Giorgi <agladzegi@gmail.com>
2019-08-22 18:13:17 +0300

    again redirect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,15 @@
 import React from "react"
-import { navigate } from "gatsby"
 
-const NotFoundPage = () => navigate('/')
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+    <Layout>
+        <SEO title="Not Found" />
+        <div className="not-found">
+            <h3>Oops, Page not found...</h3>
+        </div>
+    </Layout>
+)
 
 export default NotFoundPage
